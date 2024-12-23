ec206bacc81295af6870a58a755d6dad77a0c8df
amiyasarkaz <deividsonic2@gmail.com>
2024-12-23 12:56:45 +0100

    Rellenar Matriz
    Lo siguiente que haremos sera rellenar la matriz con numeros aleatorios del 1 al 100.

--- src/MatrizApp/MatrizApp.java
+++ src/MatrizApp/MatrizApp.java
@@ -13,6 +13,14 @@ public class MatrizApp {
         int[][] matriz = new int[filas][columnas];
         
         Random rand = new Random();
+        
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = rand.nextInt(100) + 1;
+                
+            }
+            
+        }
 	}
 
 }
