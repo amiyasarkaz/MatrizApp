48a36e9f82993418af54bec805d6cf0b15072530
amiyasarkaz <deividsonic2@gmail.com>
2024-12-23 12:55:40 +0100

    Declarar Matriz
    Lom primero que haremos sera crear la matriz.

--- src/MatrizApp/MatrizApp.java
+++ src/MatrizApp/MatrizApp.java
@@ -1,10 +1,18 @@
 package MatrizApp;
 
+import java.util.Random;
+
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int filas = 5;
+        int columnas = 5;
+        int[][] matriz = new int[filas][columnas];
+        
+        Random rand = new Random();
 	}
 
 }
