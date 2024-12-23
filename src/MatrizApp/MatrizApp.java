1ad337c9fe98d5a799907fd59a5fbbabf02adc42
amiyasarkaz <deividsonic2@gmail.com>
2024-12-23 13:06:56 +0100

    Preguntar Usuario
    Lo primero que haremos sera preguntar al usuario que ingrese un numero para buscarlo.

--- src/MatrizApp/MatrizApp.java
+++ src/MatrizApp/MatrizApp.java
@@ -1,10 +1,44 @@
 package MatrizApp;
 
+import java.util.Scanner;
+
 public class MatrizApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("\nIngrese el número a buscar: ");
+        int buscar = scanner.nextInt();
+        
+       
+        
 
+}
+	
 }
